3de5a2558345b01ee08154fec746bdc623f730d4
Isaac Santos <goncalvessantosisaac@gmail.com>
2022-11-16 10:25:17 -0300

    Other components

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,5 @@
-import icon from "../../assets/img/notification_icon.svg"
-import "./styles.css"
+import icon from '../../assets/img/notification_icon.svg'
+import './styles.css'
 
 function NotificationButton() {
     return (
